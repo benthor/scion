benthor/scion

commit 2527243d7afae04c0db7c8aa85e66b7585cc9a7c

Remove some python code (#3404)
Removes unused python code and dependencies that are not needed.

python/lib/socket.py
@@ -18,19 +18,11 @@
 # Stdlib
 import logging
 import os
-import selectors
 import struct
-from abc import abstractmethod
-from errno import EHOSTUNREACH, ENETUNREACH
 from socket import (
-    AF_INET,
-    AF_INET6,
     AF_UNIX,
     MSG_DONTWAIT,
-    SOCK_DGRAM,
     SOCK_STREAM,
-    SOL_SOCKET,
-    SO_REUSEADDR,
     socket,
 )
 
@@ -38,143 +30,15 @@ from socket import (
 from external import ipaddress
 
 # SCION
-from lib.defines import SCION_BUFLEN
 from lib.dispatcher import reg_dispatcher
 from lib.errors import SCIONIOError
 from lib.packet.host_addr import haddr_get_type, haddr_parse_interface
-from lib.packet.scmp.errors import SCMPUnreachHost, SCMPUnreachNet
 from lib.util import recv_all
 from lib.thread import kill_self
 from lib.types import AddrType
 
 
-class Socket(object):
-    """
-    Base class for socket wrappers
-    """
-    @abstractmethod
-    def bind(self, addr, *args, **kwargs):
-        raise NotImplementedError
-
-    @abstractmethod
-    def send(self, data, dst=None):
-        raise NotImplementedError
-
-    @abstractmethod
-    def recv(self, block=True):
-        raise NotImplementedError
-
-    def close(self):  # pragma: no cover
-        """
-        Close the socket.
-        """
-        self.sock.close()
-
-    def settimeout(self, timeout):  # pragma: no cover
-        prev = self.sock.gettimeout()
-        self.sock.settimeout(timeout)
-        return prev
-
-    def is_active(self):
-        return True
-
-
-class UDPSocket(Socket):
-    """
-    Thin wrapper around BSD/POSIX UDP sockets.
-    """
-    def __init__(self, bind=None, addr_type=AddrType.IPV6, reuse=False):
-        """
-        Initialize a UDP socket, then call superclass init for socket options
-        and binding.
-
-        :param tuple bind:
-            Optional tuple of (`str`, `int`, `str`) describing respectively the
-            address and port to bind to, and an optional description.
-        :param addr_type:
-            Socket domain. Must be one of :const:`~lib.types.AddrType.IPV4`,
-            :const:`~lib.types.AddrType.IPV6` (default).
-        :param reuse:
-            Boolean value indicating whether SO_REUSEADDR option should be set.
-        """
-        assert addr_type in (AddrType.IPV4, AddrType.IPV6)
-        self._addr_type = addr_type
-        af_domain = AF_INET6
-        if self._addr_type == AddrType.IPV4:
-            af_domain = AF_INET
-        self.sock = socket(af_domain, SOCK_DGRAM)
-        if reuse:
-            self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.port = None
-        if bind:
-            self.bind(*bind)
-        self.active = True
-
-    def bind(self, addr, port=0, desc=None):
-        """
-        Bind socket to the specified address & port. If `addr` is ``None``, the
-        socket will bind to all interfaces.
-
-        :param str addr: Address to bind to (can be ``None``, see above).
-        :param int port: Port to bind to.
-        :param str desc: Optional purpose of the port.
-        """
-        if addr is None:
-            addr = "::"
-            if self._addr_type == AddrType.IPV4:
-                addr = ""
-        try:
-            self.sock.bind((addr, port))
-        except OSError as e:
-            logging.critical("Error binding to [%s]:%s: %s", addr, port, e)
-            kill_self()
-        self.port = self.sock.getsockname()[1]
-        if desc:
-            logging.debug("%s bound to %s:%d", desc, addr, self.port)
-
-    def send(self, data, dst=None):
-        """
-        Send data to a specified destination.
-
-        :param bytes data: Data to send.
-        :param tuple dst:
-            Tuple of (`str`, `int`) describing the destination address and port,
-            respectively.
-        """
-        try:
-            ret = self.sock.sendto(data, dst)
-        except OSError as e:
-            errno = e.args[0]
-            logging.error("Error sending %dB to %s: %s", len(data), dst, e)
-            if errno == ENETUNREACH:
-                raise SCMPUnreachNet(dst)
-            elif errno == EHOSTUNREACH:
-                raise SCMPUnreachHost(dst)
-            return False
-        if ret != len(data):
-            logging.error("Wanted to send %dB, only sent %dB", len(data), ret)
-            return False
-        return True
-
-    def recv(self, block=True):
-        """
-        Read data from socket.
-
-        :returns:
-            Tuple of (`bytes`, (`str`, `int`) containing the data, and remote
-            host/port respectively.
-        """
-        flags = 0
-        if not block:
-            flags = MSG_DONTWAIT
-        while True:
-            try:
-                return self.sock.recvfrom(SCION_BUFLEN, flags)
-            except InterruptedError:
-                pass
-
-
-class ReliableSocket(Socket):
+class ReliableSocket(object):
     """
     Wrapper around Unix socket with message framing functionality baked in
     """
@@ -302,7 +166,10 @@ class ReliableSocket(Socket):
         return packet, sender
 
     def close(self):
-        super().close()
+        """
+        Close the socket.
+        """
+        self.sock.close()
         if not self.addr:
             return
         try:
@@ -310,52 +177,3 @@ class ReliableSocket(Socket):
         except OSError as e:
             logging.critical("Error unlinking unix socket: %s", e)
             kill_self()
-
-
-class SocketMgr(object):
-    """
-    :class:`Socket` manager.
-    """
-    def __init__(self):  # pragma: no cover
-        self._sel = selectors.DefaultSelector()
-
-    def add(self, sock, callback):  # pragma: no cover
-        """
-        Add new socket.
-
-        :param UDPSocket sock: UDPSocket to add.
-        """
-        if not sock.is_active():
-            return
-        self._sel.register(sock.sock, selectors.EVENT_READ, (sock, callback))
-
-    def remove(self, sock):  # pragma: no cover
-        """
-        Remove socket.
-
-        :param UDPSocket sock: UDPSocket to remove.
-        """
-        self._sel.unregister(sock.sock)
-
-    def select_(self, timeout=None):
-        """
-        Return the set of UDPSockets that have data pending.
-
-        :param float timeout:
-            Number of seconds to wait for at least one UDPSocket to become
-            ready. ``None`` means wait forever.
-        """
-        for key, _ in self._sel.select(timeout=timeout):
-            yield key.data
-
-    def close(self):
-        """
-        Close all sockets.
-        """
-        mapping = self._sel.get_map()
-        if mapping:
-            for entry in list(mapping.values()):
-                sock = entry.data[0]
-                self.remove(sock)
-                sock.close()
-        self._sel.close()

.buildkite/scripts/build_scion_img
@@ -2,7 +2,7 @@
 
 set -e
 
-BASE_IMG=${BASE_IMG:-5596e04239d41b0aee02c0804f72197a56feb44a85da09e998d92dcd9690f52b}
+BASE_IMG=${BASE_IMG:-0205ea7cec1b767e10117a341730b08907233c5351e8ed0d7ca135ea8a5caa59}
 
 docker pull scionproto/scion_base@sha256:$BASE_IMG
 docker tag scionproto/scion_base@sha256:$BASE_IMG scion_base:latest

env/pip3/licenses/BSD-3-Clause
@@ -1,33 +1,0 @@
-PyDbLite
-Copyright (c) 2009, Pierre Quentel pierre.quentel@gmail.com
-All rights reserved.
-
-pockets
-Copyright (c) 2018 the Pockets team, see AUTHORS.
-Copyright (c) 2014 Applied Security.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice,
-       this list of conditions and the following disclaimer.
-
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-
-    3. Neither the name of the copyright holder nor the names of its contributors
-       may be used to endorse or promote products derived from this software
-       without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

python/external/stacktracer.py
@@ -1,115 +1,0 @@
-"""Stack tracer for multi-threaded applications.
-
-
-Usage:
-
-import stacktracer
-stacktracer.start_trace("trace.html",interval=5,auto=True) # Set auto flag to always update file!
-....
-stacktracer.stop_trace()
-"""
-# Source: http://code.activestate.com/recipes/577334-how-to-debug-deadlocked-multi-threaded-programs/
-
-
-from datetime import datetime, timezone
-import sys
-import threading
-import traceback
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
-
-from lib.thread import thread_safety_net
- 
- # Taken from http://bzimmer.ziclix.com/2008/12/17/python-thread-dumps/
- 
-def stacktraces():
-    # First map thread IDs to thread names
-    id_name = {}
-    for thread in threading.enumerate():
-        id_name[thread.ident] = thread.name
-    code = []
-    for threadId, stack in sys._current_frames().items():
-        code.append("\n# ThreadID: %s, Name: %s" %
-                    (threadId, id_name.get(threadId, "UNKNOWN")))
-        for filename, lineno, name, line in traceback.extract_stack(stack):
-            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
-            if line:
-                code.append("  %s" % (line.strip()))
- 
-    return highlight("\n".join(code), PythonLexer(), HtmlFormatter(
-      full=False,
-      # style="native",
-      noclasses=True,
-    ))
-
-
-# This part was made by nagylzs
-import os
-import time
-import threading
-
-class TraceDumper(threading.Thread):
-    """Dump stack traces into a given file periodically."""
-    def __init__(self,fpath,interval,auto):
-        """
-        @param fpath: File path to output HTML (stack trace file)
-        @param auto: Set flag (True) to update trace continuously.
-            Clear flag (False) to update only if file not exists.
-            (Then delete the file to force update.)
-        @param interval: In seconds: how often to update the trace file.
-        """
-        assert(interval>0.1)
-        self.auto = auto
-        self.interval = interval
-        self.fpath = os.path.abspath(fpath)
-        self.stop_requested = threading.Event()
-        threading.Thread.__init__(self, name="TraceDumper")
-    
-    def run(self):
-        thread_safety_net(self._run)
-
-    def _run(self):
-        while not self.stop_requested.isSet():
-            time.sleep(self.interval)
-            if self.auto or not os.path.isfile(self.fpath):
-                self.stacktraces()
-    
-    def stop(self):
-        self.stop_requested.set()
-        self.join()
-        try:
-            if os.path.isfile(self.fpath):
-                os.unlink(self.fpath)
-        except:
-            pass
-    
-    def stacktraces(self):
-        fout = open(self.fpath,"wb+")
-        dt = datetime.now(tz=timezone.utc)
-        try:
-            fout.write(bytes("Generated at %s" % dt.isoformat(' '), "UTF-8"))
-            fout.write(bytes(stacktraces(), "UTF-8"))
-        finally:
-            fout.close()
-
-
-_tracer = None
-def trace_start(fpath,interval=5,auto=True):
-    """Start tracing into the given file."""
-    global _tracer
-    if _tracer is None:
-        _tracer = TraceDumper(fpath,interval,auto)
-        _tracer.setDaemon(True)
-        _tracer.start()
-    else:
-        raise Exception("Already tracing to %s"%_tracer.fpath)
-
-def trace_stop():
-    """Stop tracing."""
-    global _tracer
-    if _tracer is None:
-        raise Exception("Not tracing, cannot stop.")
-    else:
-        _trace.stop()
-        _trace = None

python/lib/config.py
@@ -1,77 +1,0 @@
-# Copyright 2014 ETH Zurich
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-:mod:`config` --- SCION configuration parser
-============================================
-"""
-
-# SCION
-from lib.defines import DEFAULT_SEGMENT_TTL
-from lib.util import load_yaml_file
-
-
-class Config(object):
-    """
-    The Config class parses the configuration file of an AS and stores such
-    information for further use.
-
-    :ivar int propagation_time: the interval at which PCBs are propagated.
-    :ivar int registration_time: the interval at which paths are registered.
-    :ivar int registers_paths: whether or not the AS registers paths.
-    :ivar int cert_ver: initial version of the certificate chain.
-    :ivar int segment_ttl: the TTL of path segments registered by this AS (in seconds).
-    """
-
-    def __init__(self):  # pragma: no cover
-        self.propagation_time = 0
-        self.registration_time = 0
-        self.registers_paths = 0
-        self.cert_ver = 0
-        self.segment_ttl = 0
-
-    @classmethod
-    def from_file(cls, config_file):  # pragma: no cover
-        """
-        Create a Config instance from the configuration file.
-
-        :param str config_file: path to the configuration file
-        :returns: the newly created Config instance
-        :rtype: :class:`Config`
-        """
-        return cls.from_dict(load_yaml_file(config_file))
-
-    @classmethod
-    def from_dict(cls, config_dict):  # pragma: no cover
-        """
-        Create a Config instance from the dictionary.
-
-        :param dict config_dict: dictionary representation of configuration
-        :returns: the newly created Config instance
-        :rtype: :class:`Config`
-        """
-        config = cls()
-        config.parse_dict(config_dict)
-        return config
-
-    def parse_dict(self, config):
-        """
-        Parse a configuration file and populate the instance's attributes.
-
-        :param dict config: the name of the configuration file.
-        """
-        self.propagation_time = config['PropagateTime']
-        self.registration_time = config['RegisterTime']
-        self.registers_paths = config['RegisterPath']
-        self.cert_ver = config['CertChainVersion']
-        self.segment_ttl = config.get('PathSegmentTTL', DEFAULT_SEGMENT_TTL)

python/lib/crypto/asymcrypto.py
@@ -15,10 +15,6 @@
 :mod:`asymcrypto` --- SCION asymmetric crypto functions
 =======================================================
 """
-# stdlib
-import base64
-import os
-
 # External
 from nacl.exceptions import BadSignatureError
 from nacl.public import Box, PrivateKey, PublicKey
@@ -26,77 +22,7 @@ from nacl.signing import SigningKey, VerifyKey
 from nacl.utils import random
 
 # SCION
-from lib.crypto.util import KEYS_DIR
 from lib.errors import SCIONVerificationError
-from lib.util import read_file
-
-
-def get_sig_key_file_path(conf_dir):
-    """
-    Return the signing key seed file path.
-    """
-    return os.path.join(conf_dir, KEYS_DIR, "as-sig.seed")
-
-
-def get_sig_key(conf_dir):
-    """
-    Return the raw signing key.
-
-    :rtype: bytes
-    """
-    return base64.b64decode(read_file(get_sig_key_file_path(conf_dir)))
-
-
-def get_core_sig_key_file_path(conf_dir):
-    """
-    Return the core signing key seed file path.
-    """
-    return os.path.join(conf_dir, KEYS_DIR, "core-sig.seed")
-
-
-def get_core_sig_key(conf_dir):
-    """
-    Return the raw core signing key.
-
-    :rtype: bytes
-    """
-    return base64.b64decode(read_file(get_core_sig_key_file_path(conf_dir)))
-
-
-def get_enc_key_file_path(conf_dir):
-    """
-    Return the encryption key file path.
-    """
-    return os.path.join(conf_dir, KEYS_DIR, "as-decrypt.key")
-
-
-def get_enc_key(conf_dir):
-    """
-    Return the raw private key.
-    :rtype: bytes
-    """
-    return base64.b64decode(read_file(get_enc_key_file_path(conf_dir)))
-
-
-def generate_sign_keypair():
-    """
-    Generate Ed25519 keypair.
-
-    :returns: a pair containing the verifying (public) key and the signing (private) key.
-    :rtype: (bytes, bytes)
-    """
-    sk = SigningKey.generate()
-    return sk.verify_key.encode(), sk.encode()
-
-
-def generate_enc_keypair():
-    """
-    Generate Curve25519 keypair
-
-    :returns tuple: A byte pair containing the public key and the private key, used for encryption.
-    """
-    private_key = PrivateKey.generate()
-    return private_key.public_key.encode(), private_key.encode()
 
 
 def sign(msg, signing_key):

python/lib/crypto/certificate_chain.py
@@ -17,63 +17,18 @@
 """
 # Stdlib
 import json
-import os
 
 # External
 import lz4
 
 # SCION
-from lib.crypto.asymcrypto import verify
 from lib.crypto.certificate import Certificate
 from lib.crypto.trc import (
     ONLINE_KEY_STRING,
-    TRC,
 )
-from lib.crypto.util import CERT_DIR
 from lib.errors import SCIONVerificationError, SCIONParseError
 from lib.packet.scion_addr import ISD_AS
 from lib.util import iso_timestamp
-
-
-def get_cert_chain_file_path(conf_dir, isd_as, version):  # pragma: no cover
-    """
-    Return the certificate chain file path for a given ISD.
-    """
-    return os.path.join(conf_dir, CERT_DIR, 'ISD%s-AS%s-V%s.crt' %
-                        (isd_as.isd_str(), isd_as.as_file_fmt(), version))
-
-
-def verify_chain_trc(subject, chain, trc):
-    """
-    Verify the certificate chain for subject.
-    """
-    subject = str(subject)
-    try:
-        chain.verify(subject, trc)
-    except SCIONVerificationError as e:
-        raise SCIONVerificationError("The certificate chain verification failed:\n%s" % e)
-
-
-def verify_sig_chain_trc(msg, sig, subject, chain, trc):
-    """
-    Verify whether the packed message with attached signature is validly
-    signed by a particular subject belonging to a valid certificate chain.
-
-    :param bytes msg: message corresponding to the given signature.
-    :param bytes sig: signature computed on msg.
-    :param ISD_AS subject: signer identity.
-    :param CertificateChain chain: Certificate chain containing the signing entity's certificate.
-    :param TRC trc: Issuing TRC containing all root of trust certificates for one ISD.
-
-    :raises: SCIONVerificationError if the verification fails.
-    """
-    assert isinstance(chain, CertificateChain), type(chain)
-    assert isinstance(trc, TRC), type(trc)
-    verify_chain_trc(subject, chain, trc)
-    verifying_key = chain.as_cert.subject_sig_key_raw
-    if not verifying_key:
-        raise SCIONVerificationError("Signer's public key has not been found: %s" % subject)
-    verify(msg, sig, verifying_key)
 
 
 class CertificateChain(object):

python/lib/crypto/symcrypto.py
@@ -23,10 +23,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers.algorithms import AES
 from cryptography.hazmat.primitives.cmac import CMAC
 
-# SCION
-from lib.errors import SCIONTypeError
-from lib.types import HashType
-
 
 def mac(key, msg):
     """
@@ -70,12 +66,3 @@ def sha256(data):
 
 # Default hash function
 crypto_hash = sha256
-
-
-def hash_func_for_type(type):
-    """
-    Returns a callable corresponding to 'type'.
-    """
-    if type == HashType.SHA256:
-        return sha256
-    raise SCIONTypeError("Unknown hash function type.")

commit 34102624cd2174fafcf2f3267adf9d2d91630a21

Readd settimeout to ReliableSocket (#3414)
Fixes bug introduced in #3404

python/lib/socket.py
@@ -177,3 +177,8 @@ class ReliableSocket(object):
         except OSError as e:
             logging.critical("Error unlinking unix socket: %s", e)
             kill_self()
+
+    def settimeout(self, timeout):  # pragma: no cover
+        prev = self.sock.gettimeout()
+        self.sock.settimeout(timeout)
+        return prev